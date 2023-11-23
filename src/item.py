3bea87b13a454b3d44d3b2399953684bb2b08986
Gelena Bezusco <helenatcitok@gmail.com>
2023-11-23 09:47:50 +0000

    add new foo

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +8,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: float) -> None:
         """
         Создание экземпляра класса item.
 
@@ -17,6 +20,23 @@ class Item:
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """Геттер для получения значения атрибута name."""
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        """
+               Сеттер для установки значения атрибута name.
+
+               :param value: Новое значение для name.
+               """
+        if len(value) <= 10:
+            self._name = value[:10]
+        else:
+            raise ValueError("Длина наименования товара превышает 10 символов.")
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +51,36 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, filename: str) -> None:
+        """
+        Создает объекты Item из данных CSV-файла.
+
+        :param filename: Имя CSV-файла.
+        """
+        with open(filename, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                name = row[0]
+                price = cls.string_to_number(row[1])
+                quantity = cls.string_to_number(row[2])
+                item = Item(name, price, quantity)
+                cls.all.append(item)
+
+    @staticmethod
+    def string_to_number(value: str, default: float = 0.0, raise_error: bool = False) -> float:
+        """
+        Преобразует строку в число.
+
+        :param value: Строковое представление числа.
+        :param default: Значение по умолчанию, возвращаемое при ошибке преобразования.
+        :param raise_error: Флаг, указывающий, следует ли выбрасывать исключение при ошибке.
+        :return: Преобразованное число.
+        """
+        try:
+            return float(value)
+        except ValueError:
+            if raise_error:
+                raise  # выбросить исключение при неудачном преобразовании
+            return default
--- tests/test_item.py
+++ tests/test_item.py
@@ -15,3 +15,17 @@ def test_apply_discount(first_item):
 
 def test_calculate_total_price(first_item):
     assert first_item.calculate_total_price() == 299.98
+
+
+def test_name_setter_length_limit():
+    item = Item("ShortName", 99.99, 1)
+    assert item.name == "ShortName"
+
+    with pytest.raises(ValueError, match="Длина наименования товара превышает 10 символов."):
+        item.name = "TooLongNameToExceedTheLimit"
+
+
+def test_instantiate_from_csv():
+    csv_data = "Phone,199.99,3\nLaptop,899.99,1\n"
+    with open("test_items.csv", "w", encoding="utf-8") as csv_file:
+        csv_file.write(csv_data)
